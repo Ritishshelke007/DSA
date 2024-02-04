975d3eb7136f3d4374b05f9868f98fd36d721693
Ritish Shelke <89585029+Ritishshelke007@users.noreply.github.com>
2024-02-04 19:27:02 +0530

    Create Closest_pair_from_two_arrays.java

---
+++ Two Pointer/Closest_pair_from_two_arrays.java
@@ -0,0 +1,31 @@
+class Solution{
+    // Function for finding maximum and value pair
+    public static ArrayList<Integer> printClosest (int arr[], int brr[], int n, int m, int x) {
+       
+       int i = 0, j = m-1, ans = Integer.MAX_VALUE, a = 0, b = 0;
+       ArrayList<Integer> list = new ArrayList<>();
+       
+       while (i<n && j>=0){
+           
+           if ( Math.abs( arr[i]+brr[j]-x ) < ans ){
+               ans = Math.abs( arr[i]+brr[j]-x );
+               a = arr[i];
+               b = brr[j];
+           }
+           
+           if (arr[i]+brr[j]-x < 0){
+               i++;
+           }
+           else if (arr[i]+brr[j]-x > 0){
+               j--;
+           }
+           else {
+               break;
+           }
+       }
+       
+       list.add(a);
+       list.add(b);
+       return list;
+    }
+}
